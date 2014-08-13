d7eb95329e0571c0c5af389c1fa9eb231a439c7f
stevebifolchi <STEPHENBIFOLCHI@GMAIL.COM>
2014-08-13 16:31:40 -0400

    exercise2

---
+++ exercise2.rb
@@ -0,0 +1,11 @@
+puts 55 * 0.15
+
+#total will tell you how much you 15% tip is on a $55 meal
+
+puts "Hello Bitmaker" +10.to_s #adding string to an integer
+
+
+num = 45628 * 7839
+puts "Your grand total is #{num}!" #result of math question used in a string
+
+puts (true && false) || (false && true) || !(false && false)
